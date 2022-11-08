eb92f9d3a44e16b468b8cad9a817bbf45ca36890
Jillu Rahman Jibon <jillurahmanjibon@gmail.com>
2022-11-08 14:17:06 +0600

    footer update

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -5,26 +5,26 @@ const Navbar = () => {
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
 	return (
 		<div>
-			<div class="bg-gray-900">
-				<div class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-					<div class="relative flex items-center justify-between">
+			<div className="bg-gray-900">
+				<div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+					<div className="relative flex items-center justify-between">
 						<Link
 							to="/"
 							aria-label="Food&Masty"
 							title="Food&Masty"
-							class="inline-flex items-center">
+							className="inline-flex items-center">
 							<img className="w-10" src={logo} alt="" />
-							<span class="ml-2 text-xl font-bold tracking-wide text-gray-100 uppercase">
+							<span className="ml-2 text-xl font-bold tracking-wide text-gray-100 uppercase">
 								Food&Masty
 							</span>
 						</Link>
-						<ul class="flex items-center hidden space-x-8 lg:flex">
+						<ul className="flex items-center hidden space-x-8 lg:flex">
 							<li>
 								<NavLink
 									to="/home"
 									aria-label="Home"
 									title="Home"
-									class="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400">
+									className="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400">
 									Home
 								</NavLink>
 							</li>
@@ -33,7 +33,7 @@ const Navbar = () => {
 									to="/recipes"
 									aria-label="Our Recipes"
 									title="Our Recipes"
-									class="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400">
+									className="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400">
 									Recipes
 								</NavLink>
 							</li>
@@ -42,7 +42,7 @@ const Navbar = () => {
 									to="/blog"
 									aria-label="Blog"
 									title="Blog"
-									class="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400">
+									className="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400">
 									Blog
 								</NavLink>
 							</li>
@@ -51,29 +51,29 @@ const Navbar = () => {
 									to=""
 									aria-label="About us"
 									title="About us"
-									class="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400">
+									className="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400">
 									About us
 								</NavLink>
 							</li>
 						</ul>
-						<ul class="flex items-center hidden space-x-8 lg:flex">
+						<ul className="flex items-center hidden space-x-8 lg:flex">
 							<li>
 								<NavLink
 									to=""
-									class="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+									className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
 									aria-label="Sign up"
 									title="Sign up">
 									Sign up
 								</NavLink>
 							</li>
 						</ul>
-						<div class="lg:hidden">
+						<div className="lg:hidden">
 							<button
 								aria-label="Open Menu"
 								title="Open Menu"
-								class="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline"
+								className="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline"
 								onClick={() => setIsMenuOpen(true)}>
-								<svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+								<svg className="w-5 text-gray-600" viewBox="0 0 24 24">
 									<path
 										fill="currentColor"
 										d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
@@ -89,16 +89,16 @@ const Navbar = () => {
 								</svg>
 							</button>
 							{isMenuOpen && (
-								<div class="absolute top-0 left-0 w-full">
-									<div class="p-5 bg-white border rounded shadow-sm">
-										<div class="flex items-center justify-between mb-4">
+								<div className="absolute top-0 left-0 w-full">
+									<div className="p-5 bg-white border rounded shadow-sm">
+										<div className="flex items-center justify-between mb-4">
 											<div>
 												<NavLink
 													to=""
 													aria-label="Food&Masty"
 													title="Food&Masty"
-													class="inline-flex items-center">
-													<span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
+													className="inline-flex items-center">
+													<span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
 														Food&Masty
 													</span>
 												</NavLink>
@@ -107,9 +107,11 @@ const Navbar = () => {
 												<button
 													aria-label="Close Menu"
 													title="Close Menu"
-													class="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+													className="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
 													onClick={() => setIsMenuOpen(false)}>
-													<svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+													<svg
+														className="w-5 text-gray-600"
+														viewBox="0 0 24 24">
 														<path
 															fill="currentColor"
 															d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
@@ -119,13 +121,13 @@ const Navbar = () => {
 											</div>
 										</div>
 										<nav>
-											<ul class="space-y-4">
+											<ul className="space-y-4">
 												<li>
 													<NavLink
 														to="/"
 														aria-label="Home"
 														title="Home"
-														class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">
+														className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">
 														Home
 													</NavLink>
 												</li>
@@ -134,7 +136,7 @@ const Navbar = () => {
 														to="/recipes"
 														aria-label="Recipes"
 														title="Recipes"
-														class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">
+														className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">
 														Recipes
 													</NavLink>
 												</li>
@@ -143,7 +145,7 @@ const Navbar = () => {
 														to="/blog"
 														aria-label="Blog"
 														title="Blog"
-														class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">
+														className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">
 														Blog
 													</NavLink>
 												</li>
@@ -152,14 +154,14 @@ const Navbar = () => {
 														to=""
 														aria-label="About us"
 														title="About us"
-														class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">
+														className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">
 														About us
 													</NavLink>
 												</li>
 												<li>
 													<NavLink
 														to=""
-														class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+														className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
 														aria-label="Sign up"
 														title="Sign up">
 														Sign up
